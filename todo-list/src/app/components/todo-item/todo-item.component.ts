90be3c122025fd20140b7a9be655fd64c6f902be
your_github_username <rabi.abdelrahman@gmail.com>
2021-02-03 00:16:48 +0200

    delete todo item functionality

--- todo-list/src/app/components/todo-item/todo-item.component.ts
+++ todo-list/src/app/components/todo-item/todo-item.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit,Input } from '@angular/core';
+import { Component, OnInit,Input,EventEmitter,Output } from '@angular/core';
 import {TodoService} from '../../services/todo.service';
 import { Todo} from 'src/app/models/Todo'
 @Component({
@@ -8,6 +8,8 @@ import { Todo} from 'src/app/models/Todo'
 })
 export class TodoItemComponent implements OnInit {
 @Input() todo:Todo
+@Output() deleteTodo:EventEmitter<Todo> = new EventEmitter()
+
   constructor(private todoService:TodoService) { }
 
   ngOnInit(): void {
@@ -32,6 +34,6 @@ export class TodoItemComponent implements OnInit {
   }
 
   onDelete(todo){
-    console.log('Delete');
+    this.deleteTodo.emit(todo)
   }
 }
